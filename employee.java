ee1837c579a74545ac98b28b31b61b8b355de015
karunagaran <karunakalaivel@gmail.com>
2017-11-15 12:03:00 +0530

    Xml using program

--- employee.java
+++ employee.java
@@ -1,13 +1,19 @@
 package org.main;
 
+/*import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="EMPLOYEE_DETAILS")*/
 public class Employee {
 	public Employee() {
 		super();
 	}
+
+	
 
 	public Employee(int id, String name, String email, long phone) {
 		super();
@@ -17,6 +23,8 @@ public class Employee {
 		this.phone = phone;
 		
 	}
+
+
 
 	public int getId() {
 		return id;
@@ -49,14 +57,67 @@ public class Employee {
 	public void setPhone(long phone) {
 		this.phone = phone;
 	}
+	 public double getBasicSalary() {
+			return basicSalary;
+		}
 
-	
+		public void setBasicSalary(int basicSalary) {
+			this.basicSalary = basicSalary;
+		}
 
+		public float getDearnessAllowance() {
+			return dearnessAllowance;
+		}
+
+		public void setDearnessAllowance(int dearnessAllowance) {
+			this.dearnessAllowance = dearnessAllowance;
+		}
+
+		public float getHouseRentAllowance() {
+			return houseRentAllowance;
+		}
+
+		public void setHouseRentAllowance(int houseRentAllowance) {
+			this.houseRentAllowance = houseRentAllowance;
+		}
+
+		public float getProvidentFund() {
+			return providentFund;
+		}
+
+		public void setProvidentFund(int providentFund) {
+			this.providentFund = providentFund;
+		}
+
+		public float getTravelAllowance() {
+			return travelAllowance;
+		}
+
+		public void setTravelAllowance(int travelAllowance) {
+			this.travelAllowance = travelAllowance;
+		}
 	
-    @Id
+   /* @Id
+    @GeneratedValue
+    @Column (name="EMP_ID")*/
 	private int id;
+   // @Column (name="EMP_NAME",nullable=true,length=255)
 	private String name;
+   
+	//@Column (name="EMP_EMAIL")
 	private String email;
+   // @Column (name="EMP_PHONE")
 	private long phone;
+   // @Column (name="BASIC_SALARY")
+	private double basicSalary;
+   // @Column (name="DA")
+	private float dearnessAllowance;
+  //  @Column (name="HRA")
+	private float houseRentAllowance;
+  //  @Column (name="PA")
+	private float providentFund;
+  //  @Column (name="TA")
+	private float travelAllowance;
+	
 	//private boolean gender;
 }
