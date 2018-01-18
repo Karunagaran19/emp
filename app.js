c76e9e378086dc34065b2413071be0d4b9825527
Karunagaran19 <33505772+Karunagaran19@users.noreply.github.com>
2018-01-18 12:31:09 +0530

    angularjs

---
+++ app.js
@@ -0,0 +1,97 @@
+(function () {
+    var app = angular.module("AJS", []);
+    app.controller("StoreController", function () {
+        this.products = items
+    });
+    var items = [{
+        name: "Soap",
+        price: 150.0,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }, {
+        name: "Shampoo",
+        price: 250.5,
+        canPurchase: true,
+        soldOut: false,
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }];
+})();
